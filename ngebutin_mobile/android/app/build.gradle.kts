dc7edd21913a44b50c7624286d4ed4365c54a108
gabrielputra-2311102223 <2311102223@ittelkom-pwt.ac.id>
2026-06-11 23:54:42 +0700

    fix: update desugar_jdk_libs version

--- ngebutin_mobile/android/app/build.gradle.kts
+++ ngebutin_mobile/android/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        coreLibraryDesugaringEnabled = true
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -47,6 +47,6 @@ flutter {
 }
 
 dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
     implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
 }
