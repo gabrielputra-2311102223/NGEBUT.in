0f8b5b482c39f3036c3dfb539bd530d458f0f208
gabrielputra-2311102223 <2311102223@ittelkom-pwt.ac.id>
2026-06-11 23:55:42 +0700

    fix: match package name with firebase config

--- ngebutin_mobile/android/app/build.gradle.kts
+++ ngebutin_mobile/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.ngebutin.ngebutin_mobile"
+    namespace = "com.example.ngebutin_mobile"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -23,7 +23,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.ngebutin.ngebutin_mobile"
+        applicationId = "com.example.ngebutin_mobile"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
